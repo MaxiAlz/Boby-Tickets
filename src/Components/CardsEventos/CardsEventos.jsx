69e46ad60bc42aefa12ec8bbb9e0be6eb053c862
Maxi Alzugaray <maxialz99@hotmail.com>
2022-07-15 19:30:18 -0300

    terminando diseño de cards

--- src/Components/CardsEventos/CardsEventos.jsx
+++ src/Components/CardsEventos/CardsEventos.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import BotonTipoPublico from '../BotonTipoPublico/BotonTipoPublico'
 
 import './CardEventos.css'
 const CardsEventos = ({ evento }) => {
-  const { images, titulo, ubicacion, entradas, fecha } = evento
+  const { images, titulo, ubicacion, entradas, fecha, tipoEvento } = evento
 
   let min = 9999999999999
   let flag = min
@@ -21,19 +22,20 @@ const CardsEventos = ({ evento }) => {
   return (
     <div className="card mx-2 my-2 g-0 shadow col-3 cardContainer">
       <img src={images.post} className="img-fluid" alt={titulo} />
-      <div className='textoCard container m-0'>
-      {/* ${fecha.dia}/${fecha.mes} | ${fecha.horario.hora}: ${fecha.horario.minutos} Hs */}
-        <h5 className=" text-white m-0">{`${titulo} en ${ubicacion.provincia} | ${fecha.dia}/${fecha.mes}`}</h5>
+      <div className='textoCard container m-0 d-flex align-items-center'>
+        {/* ${fecha.dia}/${fecha.mes} | ${fecha.horario.hora}: ${fecha.horario.minutos} Hs */}
+        <h5 className="text-white text-center m-0">{`${titulo} | ${ubicacion.provincia} | ${fecha.dia}/${fecha.mes}`}</h5>
       </div>
       <div className="">
         {
           flag != min ?
-            <a href="" className="btn container ">{`ENTRADAS DESDE:  $ ${min}`}</a>
+            <a href="" className="btn container btnPrecioEntradas">{`ENTRADAS DESDE:  $ ${min}`}</a>
             :
             //CAMBIAR BOTON A ROJO          
-            <a href="" className="btn container btn-danger disabled">{`ENTRADAS AGOTADAS`}</a>
+            <a href="" className="btn container btn-danger disabled btnPrecioEntradas">{`ENTRADAS AGOTADAS`}</a>
         }
       </div>
+      <BotonTipoPublico tipoEvento={tipoEvento} />
     </div>
   )
 }
